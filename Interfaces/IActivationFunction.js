d2bd56c324fd4688428c35562f30cc58478332f2
maxim-polyakov <maxim7012@mail.ru>
2024-01-26 22:51:33 +0300

    JS Classes

---
+++ Interfaces/IActivationFunction.js
@@ -0,0 +1,12 @@
+class TeslaModelS {
+    /// <returnssd>The params for this activation function.</returns>
+    Params;
+    /// <returns>The names of the parameters.</returns>
+    ParamNames;
+    //void ActivationFunction(double[] d, int start, int size);
+    //double DerivativeFunction(double b, double a);
+
+
+    /// <returns>Return true if this function has a derivative.</returns>
+    HasDerivative();
+}
